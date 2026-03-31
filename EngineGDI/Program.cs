9073ad7b2c23df04225af03ee966530ebcda3b59
ashadowreset <juanpablocarracedo@gmail.com>
2026-03-31 19:06:56 -0300

    Primer commit: proyecto inicial

---
+++ EngineGDI/Program.cs
@@ -0,0 +1,127 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace EngineGDI
+{
+    static class Program
+    {
+        // mostrar debug
+        public static bool showDebug = true;
+        public static string currentMsg = "";
+
+        //Posicion Player
+        public static float polloX = 0;
+        public static float polloY = 500;
+        public static float polloJumpX = 0;
+        public static float polloJumpY = 0;
+        public static float gravity = 0;
+        public static float deltaTime;
+
+        static DateTime lastFrameTime = DateTime.Now;
+
+        //Tamaño del juego
+        public static int SCREEN_WIDTH = 1024;
+        public static int SCREEN_HEIGHT = 780;
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Engine.Initialize("IERVA ENGINE", SCREEN_WIDTH, SCREEN_HEIGHT, false);
+
+         
+
+            while (Engine.IsWindowOpen)
+            {
+                #region Engine Window Control
+                Engine.UpdateWindow();
+                #endregion
+
+                calcDeltatime();
+                input();
+                update();
+                draw();
+
+
+                #region Engine Window Control
+                Engine.Clear(Color.Black);
+                // mensajes de debug
+                if (showDebug)
+                {
+                    Engine.ClearDebug();
+                    Engine.DebugLog(currentMsg);
+
+                }
+                Engine.Window.Invalidate();
+                #endregion
+            }
+
+        }
+
+        static void draw()
+        {
+            Engine.Draw("Fondo.png", 0, 0);
+            Engine.Draw("Pollo.png", polloX, polloY, 0.1f, 0.1f);
+        }
+
+       static void update()
+       {
+            
+          polloY = polloY + gravity;
+          if (polloY >= 630)
+            {
+                gravity = 0;
+            }
+          else
+            {
+                gravity = 1;
+            }
+          if (polloX > 1000)
+            {
+                polloX = 0;
+            }
+            
+       }
+
+        static void input()
+        {
+            if (Engine.IsKeyDown(Keys.W))
+            {
+                polloY = polloY - 5;
+            }
+            if (Engine.IsKeyDown(Keys.D))
+            {
+                polloX = polloX + 0.8f;
+                    
+            }
+            if ((Engine.IsKeyDown(Keys.A)))
+            {
+                polloX = polloX - 0.8f;
+            }
+            while ((Engine.IsKeyDown(Keys.Space) && (polloJumpX < 100)))
+            {
+                polloJumpX = polloJumpX + 20;
+                polloJumpY = polloJumpY + 100;
+            }
+            if (((Engine.IsKeyDown(Keys.S))))
+            {
+                polloX = polloX + polloJumpX;
+                polloY = polloY - polloJumpY;
+                polloJumpY = 0;
+                polloJumpX = 0;
+            }
+        }
+
+        static void calcDeltatime()
+        {
+            TimeSpan deltaSpan = DateTime.Now - lastFrameTime;
+            deltaTime = (float)deltaSpan.TotalSeconds;
+            lastFrameTime = DateTime.Now;
+        }
+    }
+}
---
+++ UnitTestProject1/TestGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EngineGDI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestGame
+    {
+        [TestMethod]
+        public void TestGetDamage()
+        {
+            Character c = new Character(new Vector2(0, 0), 95, 101, null);
+            c.DamageLife(5);
+
+            Assert.AreEqual(95.0f, c.Life);
+        }
+
+        [TestMethod]
+        public void TestGetDamage2()
+        {
+            Character1 c = new Character1(new Vector2(0, 0), 95, 101, null);
+            c.DamageLife(6);
+
+            Assert.AreNotEqual(95.0f, c.Life);
+        }
+    }
+}
